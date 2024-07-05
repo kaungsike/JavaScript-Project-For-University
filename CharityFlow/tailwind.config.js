1d232367f756348cce0f8cab0ca7ec7ea7585bf6
kaungsike <kaungsike9@gmail.com>
2024-07-05 15:24:07 +0630

    Code Update

--- CharityFlow/tailwind.config.js
+++ CharityFlow/tailwind.config.js
@@ -16,6 +16,15 @@ export default {
         }
       }
     },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
